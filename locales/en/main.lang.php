e623ba9e49b7e8f68e1a3efe23c8336c82eef259
franck <carnet.franck.paul@gmail.com>
2024-01-27 15:56:09 +0100

    l10n

--- locales/en/main.lang.php
+++ locales/en/main.lang.php
@@ -11,13 +11,13 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Ping Bluesky']                           = '';
-L10n::$locales['pingBluesky']                            = '';
-L10n::$locales['Bluesky']                                = '';
+L10n::$locales['Ping Bluesky'] = '';
+L10n::$locales['pingBluesky'] = '';
+L10n::$locales['Bluesky'] = '';
 L10n::$locales['All entries have been ping to Bluesky.'] = '';
-L10n::$locales['Activate pingBluesky plugin']            = '';
-L10n::$locales['Bluesky instance:']                      = '';
-L10n::$locales['Application token:']                     = '';
-L10n::$locales['Status prefix:']                         = '';
-L10n::$locales['Include tags']                           = '';
-L10n::$locales['Save']                                   = '';
+L10n::$locales['Activate pingBluesky plugin'] = '';
+L10n::$locales['Bluesky instance:'] = '';
+L10n::$locales['Application token:'] = '';
+L10n::$locales['Status prefix:'] = '';
+L10n::$locales['Include tags'] = '';
+L10n::$locales['Save'] = '';
