a2f5ba564ec18f0af2921e959543a82fbcdf71c5
franck <carnet.franck.paul@gmail.com>
2025-12-30 12:03:19 +0100

    New release 3.5

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Ping Bluesky',
     'Ping Bluesky',
     'Franck Paul',
-    '3.4',
+    '3.5',
     [
-        'date'        => '2025-12-22T10:56:01+0100',
+        'date'        => '2025-12-30T12:03:19+0100',
         'requires'    => [['core', '2.36']],
         'type'        => 'plugin',
         'permissions' => 'My',
