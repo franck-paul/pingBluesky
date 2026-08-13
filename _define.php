aadb45af8c1341a48ad146bf2ce887cd9ec7dd13
franck <contact@open-time.net>
2026-08-13 16:44:22 +0200

    Enforces some verification before registering module

--- _define.php
+++ _define.php
@@ -11,19 +11,23 @@
  * @copyright Franck Paul contact@open-time.net
  * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
  */
-$this->registerModule(
-    'Ping Bluesky',
-    'Ping Bluesky',
-    'Franck Paul',
-    '4.0.2',
-    [
-        'date'        => '2026-08-05T10:13:55+0200',
-        'requires'    => [['core', '2.39']],
-        'type'        => 'plugin',
-        'permissions' => 'My',
-        'details'     => 'https://open-time.net/docs/plugins/pingBluesky',
-        'support'     => 'https://github.com/franck-paul/pingBluesky',
-        'repository'  => 'https://raw.githubusercontent.com/franck-paul/pingBluesky/main/dcstore.xml',
-        'license'     => 'gpl2',
-    ]
-);
+declare(strict_types=1);
+
+if (isset($this) && is_object($this) && method_exists($this, 'registerModule') && isset($this->id) && is_string($this->id)) {
+    $this->registerModule(
+        'Ping Bluesky',
+        'Ping Bluesky',
+        'Franck Paul',
+        '4.0.2',
+        [
+            'date'        => '2026-08-05T10:13:55+0200',
+            'requires'    => [['core', '2.39']],
+            'type'        => 'plugin',
+            'permissions' => 'My',
+            'details'     => 'https://open-time.net/docs/plugins/pingBluesky',
+            'support'     => 'https://github.com/franck-paul/pingBluesky',
+            'repository'  => 'https://raw.githubusercontent.com/franck-paul/pingBluesky/main/dcstore.xml',
+            'license'     => 'gpl2',
+        ]
+    );
+}
